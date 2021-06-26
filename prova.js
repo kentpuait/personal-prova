c162c91ab65fa0d8925e6d6fc9ba8eeea520c7c7
Kostiantyn <84709838+kentpuait@users.noreply.github.com>
2021-06-26 19:17:52 +0200

    Add files via upload
    file prova e interpolation

---
+++ prova.js
@@ -0,0 +1,14 @@
+//alert('Hello');
+
+/* const result = confirm('Are you here?');
+console.log(result); */
+/* const answer = prompt("Hai 18 anni?", "");
+console.log(typeof(answer)); */
+
+const answer = [];
+
+answer[0] = prompt("Come ti chiami?");
+answer[1] = prompt("Quanti anni hai?");
+answer[2] = prompt("Da dove sei?");
+
+document.write(answer);
