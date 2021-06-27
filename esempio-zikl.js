5ada2f8c19ff9ad48375cbf868c5007465c05712
Constantine Kentpuait <kentpuait@gmail.com>
2021-06-27 19:45:49 +0200

    finito esempio-zikl

--- esempio-zikl.js
+++ esempio-zikl.js
@@ -20,7 +20,23 @@ for (let i = 0; i < 2; i++) {
   const a = prompt("Ultimo film?", "");
   const b = prompt("Vota?", "");
 
-  personalMovieDB.movies[a] = b;
+  if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } else {
+    console.log("Error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Quardato poco i film");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Lei spettatore clasico");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Lei cinefilo");
+} else {
+  console - log("Errore");
 }
 
 console.log(personalMovieDB);
