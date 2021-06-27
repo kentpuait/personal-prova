98d466ebc56ee00d757bfbfb1eb28ec6809ccf10
Constantine Kentpuait <kentpuait@gmail.com>
2021-06-27 20:30:01 +0200

    aggiunto testo

--- function.js
+++ function.js
@@ -8,17 +8,30 @@ function showFirstMessage(text) {
 showFirstMessage("Hello World!");
 console.log(num);
 
-function calc(a, b, c) {
+/* function calc(a, b, c) {
   return a - b + c;
-}
-console.log(calc(4, 5, 3));
+} */
+/* console.log(calc(4, 5, 3));
 console.log(calc(5, 8, 4));
 console.log(calc(10, 9, 5));
 
 function red() {
   let num = 50;
   return num;
-}
+} */
 
 const anotherNum = red();
 console.log(anotherNum);
+
+const logger = function () {
+  console.log("Hello");
+};
+logger();
+
+/* function calc (a, b) {
+  return a - b;
+} */
+const calc = (a, b) => {
+  console.log'1');
+   return a + b;
+};
