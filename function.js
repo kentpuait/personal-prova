881ce2bb7f55ceadfb697174ac1fe3f19e33010f
Constantine Kentpuait <kentpuait@gmail.com>
2021-06-27 20:16:16 +0200

    vari function

--- function.js
+++ function.js
@@ -1,1 +1,24 @@
+let num = 20;
 
+function showFirstMessage(text) {
+  console.log(text);
+  num = 10;
+}
+
+showFirstMessage("Hello World!");
+console.log(num);
+
+function calc(a, b, c) {
+  return a - b + c;
+}
+console.log(calc(4, 5, 3));
+console.log(calc(5, 8, 4));
+console.log(calc(10, 9, 5));
+
+function red() {
+  let num = 50;
+  return num;
+}
+
+const anotherNum = red();
+console.log(anotherNum);
